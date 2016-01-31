e997a3dc27825a1b0b5e93389fe4ca5c2982e746
Kelly Davis <kldavis4@gmail.com>
2016-01-31 10:52:11 -0500

    Add as_json to fix serialization issues

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,15 @@ class Event < ActiveRecord::Base
 
   #TODO need to validate the date string on an event
   alias_attribute :type, :event_type
+
+  def as_json(options = {})
+    json = {:date => date, :type => event_type, :user => user} # whatever info you want to expose
+    if message != ""
+      json[:message] = message
+    end
+    if otheruser != ""
+      json[:otheruser] = otheruser
+    end
+    json
+  end
 end
