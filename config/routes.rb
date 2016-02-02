33f7127d169c8d8fb17c1ccb54aa17acfb035d03
Kelly Davis <kldavis4@gmail.com>
2016-02-02 15:38:22 -0500

    Remove subdomain configuration

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ require 'api_constraints'
 
 EventApi::Application.routes.draw do
   # Api definition
-  namespace :api, defaults: { format: :json },
-                              constraints: { subdomain: 'api' }, path: '/'  do
+  namespace :api, defaults: { format: :json }, path: '/'  do
     scope module: :v1,
               constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
