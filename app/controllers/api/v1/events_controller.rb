d3f8701fa9671bc0f1e69f23c1dad6df8ebf70e7
Kelly Davis <kldavis4@gmail.com>
2016-02-02 14:51:55 -0500

    Implement summary endpoint and associated tests - Also fixed a problem with boundary in the index query for to / from dates

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -1,5 +1,9 @@
 require 'date'
 
+ROUND_MINS = 100
+ROUND_HOURS = 10000
+ROUND_DAYS = 1000000
+
 class Api::V1::EventsController < ApplicationController
   wrap_parameters false
   respond_to :json
@@ -47,7 +51,7 @@ class Api::V1::EventsController < ApplicationController
         return
       end
 
-      @events = Event.where(["date >= ? and date <= ?", fromDate, toDate]).sort_by &:date
+      @events = Event.where("date >= '%s' and date <= '%s'" % [fromDate.strftime('%F %T.%6N'), toDate.strftime('%F %T.%6N')]).sort_by &:date
     else
       @events = Event.all.sort_by &:date
     end
@@ -56,7 +60,77 @@ class Api::V1::EventsController < ApplicationController
   end
 
   def summary
-    render json: { status: "ok" }, status: 200
+    errors = nil
+    fromDate = nil
+    toDate = nil
+    by = 'day'
+
+    if params[:from].present? && params[:to].present?
+      begin
+        fromDate = DateTime.strptime(params[:from],'%FT%TZ')
+      rescue ArgumentError
+        errors = { errors: {from: "Invalid 'from' date"}}
+      end
+
+      begin
+        toDate = DateTime.strptime(params[:to],'%FT%TZ')
+      rescue ArgumentError
+        msg = "Invalid 'to' date"
+        if errors == nil
+          errors = { errors: { to: msg} }
+        else
+          errors[:errors]['to'] = msg
+        end
+      end
+    end
+
+    if params[:by].present?
+      by = params[:by]
+      if by != 'day' && by != 'hour' && by != 'minute'
+        msg = "Expected 'day', 'hour', or 'minute"
+        if errors == nil
+          errors = { errors: { by: msg}}
+        else
+          errors[:errors]['by'] = msg
+        end
+      end
+    end
+
+    if errors != nil
+      render json: errors, status: 400
+      return
+    end
+
+    filter = ""
+    if fromDate != nil && toDate != nil
+      filter = "where date >= '%s' and date <= '%s'" % [fromDate.strftime('%F %T.%6N'), toDate.strftime('%F %T.%6N')]
+    end
+
+    date_rounder = ROUND_DAYS
+    case by
+    when'hour'
+      date_rounder = ROUND_HOURS
+    when 'minute'
+      date_rounder = ROUND_MINS
+    end
+
+    records = Event.connection.select_all("select (cast(strftime('%Y%m%d%H%M%S',date) as integer) / #{date_rounder}) * #{date_rounder} as timeframe,"\
+                                            "sum(case when type = 'comment' then 1 else 0 end) as comments,"\
+                                            "sum(case when type = 'highfive' then 1 else 0 end) as highfives,"\
+                                            "sum(case when type = 'enter' then 1 else 0 end) as enters,"\
+                                            "sum(case when type = 'leave' then 1 else 0 end) as leaves from events " + filter + " group by timeframe")
+
+    results = Array.new()
+
+    records.each do |record|
+      #Format the timeframe into the rolled up date format and add to the results array
+      record[:date] = DateTime.strptime(record['timeframe'].to_s,'%Y%m%d%H%M%S').strftime('%FT%TZ')
+
+      #Exclude the original timeframe key
+      results.push(record.except('timeframe'))
+    end
+
+    render json: { events: results }, status: 200
   end
 
   private
--- spec/controllers/api/v1/events_controller_spec.rb
+++ spec/controllers/api/v1/events_controller_spec.rb
@@ -70,6 +70,198 @@ describe Api::V1::EventsController do
     end
   end
 
+  describe "GET #summary" do
+    context "four events over 4 days with default params" do
+      before(:each) do
+        @event1 = FactoryGirl.create :event, :date => Date.new(2016, 1, 1), :type => 'enter'
+        @event2 = FactoryGirl.create :event, :date => Date.new(2016, 1, 2), :type => 'comment'
+        @event3 = FactoryGirl.create :event, :date => Date.new(2016, 1, 3), :type => 'highfive'
+        @event4 = FactoryGirl.create :event, :date => Date.new(2016, 1, 4), :type => 'leave'
+        get :summary, format: :json
+      end
+
+      it "returns a summary with 4 dates each with one event type" do
+        events_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(events_response[:events].length).to eql 4
+
+        total_enters = 0
+        total_leaves = 0
+        total_comments = 0
+        total_highfives = 0
+        events_response[:events].each do |event|
+          total_enters = total_enters + event[:enters]
+          total_leaves = total_leaves + event[:leaves]
+          total_highfives = total_highfives + event[:highfives]
+          total_comments = total_comments + event[:comments]
+        end
+
+        expect(total_enters).to eql 1
+        expect(total_leaves).to eql 1
+        expect(total_highfives).to eql 1
+        expect(total_comments).to eql 1
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "four events over 4 days filtering across 4 days" do
+      before(:each) do
+        @event1 = FactoryGirl.create :event, :date => Date.new(2016, 1, 1), :type => 'enter'
+        @event2 = FactoryGirl.create :event, :date => Date.new(2016, 1, 2), :type => 'comment'
+        @event3 = FactoryGirl.create :event, :date => Date.new(2016, 1, 3), :type => 'highfive'
+        @event4 = FactoryGirl.create :event, :date => Date.new(2016, 1, 4), :type => 'leave'
+        get :summary, :from => '2016-01-01T00:00:00Z', :to => '2016-01-04T23:59:59Z', :by => 'day', format: :json
+      end
+
+      it "returns a summary with 4 dates each with one event type" do
+        events_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(events_response[:events].length).to eql 4
+
+        total_enters = 0
+        total_leaves = 0
+        total_comments = 0
+        total_highfives = 0
+        events_response[:events].each do |event|
+          total_enters = total_enters + event[:enters]
+          total_leaves = total_leaves + event[:leaves]
+          total_highfives = total_highfives + event[:highfives]
+          total_comments = total_comments + event[:comments]
+        end
+
+        expect(total_enters).to eql 1
+        expect(total_leaves).to eql 1
+        expect(total_highfives).to eql 1
+        expect(total_comments).to eql 1
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "four events over 4 hours" do
+      before(:each) do
+        @event1 = FactoryGirl.create :event, :date => DateTime.new(2016, 1, 1, 1, 0, 0), :type => 'enter'
+        @event2 = FactoryGirl.create :event, :date => DateTime.new(2016, 1, 1, 2, 0, 0), :type => 'comment'
+        @event3 = FactoryGirl.create :event, :date => DateTime.new(2016, 1, 1, 3, 0, 0), :type => 'highfive'
+        @event4 = FactoryGirl.create :event, :date => DateTime.new(2016, 1, 1, 4, 0, 0), :type => 'leave'
+        get :summary, :from => '2016-01-01T00:00:00Z', :to => '2016-01-01T23:59:59Z', :by => 'hour', format: :json
+      end
+
+      it "returns a summary with 4 date times each with one event type" do
+        events_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(events_response[:events].length).to eql 4
+
+        total_enters = 0
+        total_leaves = 0
+        total_comments = 0
+        total_highfives = 0
+        events_response[:events].each do |event|
+          total_enters = total_enters + event[:enters]
+          total_leaves = total_leaves + event[:leaves]
+          total_highfives = total_highfives + event[:highfives]
+          total_comments = total_comments + event[:comments]
+        end
+
+        expect(total_enters).to eql 1
+        expect(total_leaves).to eql 1
+        expect(total_highfives).to eql 1
+        expect(total_comments).to eql 1
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "four events over 4 minutes" do
+      before(:each) do
+        @event1 = FactoryGirl.create :event, :date => DateTime.new(2016, 1, 1, 4, 1, 0), :type => 'enter'
+        @event2 = FactoryGirl.create :event, :date => DateTime.new(2016, 1, 1, 4, 2, 0), :type => 'comment'
+        @event3 = FactoryGirl.create :event, :date => DateTime.new(2016, 1, 1, 4, 3, 0), :type => 'highfive'
+        @event4 = FactoryGirl.create :event, :date => DateTime.new(2016, 1, 1, 4, 4, 0), :type => 'leave'
+        get :summary, :from => '2016-01-01T00:00:00Z', :to => '2016-01-01T23:59:59Z', :by => 'minute', format: :json
+      end
+
+      it "returns a summary with 4 date times each with one event type" do
+        events_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(events_response[:events].length).to eql 4
+
+        total_enters = 0
+        total_leaves = 0
+        total_comments = 0
+        total_highfives = 0
+        events_response[:events].each do |event|
+          total_enters = total_enters + event[:enters]
+          total_leaves = total_leaves + event[:leaves]
+          total_highfives = total_highfives + event[:highfives]
+          total_comments = total_comments + event[:comments]
+        end
+
+        expect(total_enters).to eql 1
+        expect(total_leaves).to eql 1
+        expect(total_highfives).to eql 1
+        expect(total_comments).to eql 1
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "four events over 2 minutes" do
+      before(:each) do
+        @event1 = FactoryGirl.create :event, :date => DateTime.new(2016, 1, 1, 4, 1, 0), :type => 'enter'
+        @event2 = FactoryGirl.create :event, :date => DateTime.new(2016, 1, 1, 4, 1, 18), :type => 'comment'
+        @event3 = FactoryGirl.create :event, :date => DateTime.new(2016, 1, 1, 4, 4, 0), :type => 'highfive'
+        @event4 = FactoryGirl.create :event, :date => DateTime.new(2016, 1, 1, 4, 4, 15), :type => 'leave'
+        get :summary, :from => '2016-01-01T00:00:00Z', :to => '2016-01-01T23:59:59Z', :by => 'minute', format: :json
+      end
+
+      it "returns a summary with 4 date times each with one event type" do
+        events_response = JSON.parse(response.body, symbolize_names: true)
+
+        expect(events_response[:events].length).to eql 2
+
+        total_enters = 0
+        total_leaves = 0
+        total_comments = 0
+        total_highfives = 0
+        events_response[:events].each do |event|
+          total_enters = total_enters + event[:enters]
+          total_leaves = total_leaves + event[:leaves]
+          total_highfives = total_highfives + event[:highfives]
+          total_comments = total_comments + event[:comments]
+        end
+
+        expect(total_enters).to eql 1
+        expect(total_leaves).to eql 1
+        expect(total_highfives).to eql 1
+        expect(total_comments).to eql 1
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context "invalid to/from/by parameters" do
+      before(:each) do
+        get :summary, :from => 'XXXXX', :to => 'XXXX', :by => 'second', format: :json
+      end
+
+      it "renders an errors json" do
+        event_response = JSON.parse(response.body, symbolize_names: true)
+        expect(event_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on why the params are invalid" do
+        event_response = JSON.parse(response.body, symbolize_names: true)
+        expect(event_response[:errors][:to]).to eql "Invalid 'to' date"
+        expect(event_response[:errors][:from]).to eql "Invalid 'from' date"
+        expect(event_response[:errors][:by]).to eql "Expected 'day', 'hour', or 'minute"
+      end
+
+      it { should respond_with 400 }
+    end
+  end
+
   describe "POST #clear" do
     before(:each) do
       post :clear
