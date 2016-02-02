0dc23e41a249355c98a4b025431b696df121164c
Kelly Davis <kldavis4@gmail.com>
2016-02-02 15:10:42 -0500

    Cleaned up comments

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,13 +1,10 @@
 class Event < ActiveRecord::Base
+  #Required fields
   validates :user, :type, :date, :presence => true
 
   self.inheritance_column = nil
 
-  #TODO need to validate the date string on an event
-  #alias_attribute :type, :event_type
-
-  #TODO restrict invalid post body types to the ones that are specified
-
+  #Limit fields serialized to (user, type, date, message and otheruser)
   def as_json(options={})
     super(:only => [:user, :type, :date, :message, :otheruser])
   end
